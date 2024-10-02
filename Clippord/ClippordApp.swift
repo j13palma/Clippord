17cd7b9c06047ba7d0a61b519dc65210ae45a0e6
julio palma <palma@onroot.io>
2024-10-02 13:53:27 -0400

    Initial Commit

---
+++ Clippord/ClippordApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClippordApp.swift
+//  Clippord
+//
+//  Created by Julio Palma on 10/2/24.
+//
+
+import SwiftUI
+
+@main
+struct ClippordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClippordTests/ClippordTests.swift
@@ -0,0 +1,17 @@
+//
+//  ClippordTests.swift
+//  ClippordTests
+//
+//  Created by Julio Palma on 10/2/24.
+//
+
+import Testing
+@testable import Clippord
+
+struct ClippordTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
